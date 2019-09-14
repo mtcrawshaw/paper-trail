1a90ba6d86159899df920b4f8905575108d06fac
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2019-09-14 18:32:10 -0400

    Filled out skeleton for text-based interface

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 PAPERS_PATH = "data/papers.pkl"
+KEYS = ['title', 'authors', 'labels', 'date_added', 'date_read', 'link',
+        'read', 'recorded', 'parent', 'notes']
--- main.py
+++ main.py
@@ -1,9 +1,81 @@
 from utils import load_papers, save_papers
+from config import KEYS
+
+def print_action_prompt():
+
+    msg = "Enter the number corresponding to an action!\n"
+    msg += "(1) Print list of papers with information.\n"
+    msg += "(2) Add a paper to the list.\n"
+    msg += "(3) Remove a paper from the list.\n"
+    msg += "(4) Revert back to original list of papers.\n"
+    msg += "(Anything else) Quit program.\n\n"
+    msg += "Action: "
+    print(msg, end="")
+
 
 def main():
     """ Main function to run Paper Trail program. """
 
     papers = load_papers()
+    originalPapers = list(papers)
+
+    while True:
+
+        print_action_prompt()
+        action = input()
+
+        if action == '1':
+
+            # Print papers
+            for paper in papers:
+                for i, (key, value) in enumerate(paper.items()):
+                    print("%s: %s" % (key, str(value)))
+                print("")
+
+        elif action == '2':
+
+            # Collect paper information
+            print("Enter paper information!")
+            paper = {}
+            for key in KEYS:
+                print("%s: ", end="")
+                paper[key] = input()
+
+            # Add paper to database
+            papers.append(paper)
+            print("Added paper!")
+
+        elif action == '3':
+
+            # Get name of paper to delete
+            print("Name of paper: ", end="")
+            paperName = input()
+            targetPaper = None
+
+            # Search for target paper
+            for i, paper in enumerate(papers):
+                if paperName == paper['name']:
+                    # delete from papers
+                    targetPaper = i
+                    break
+
+            # Remove paper
+            if foundPaper is None:
+                print("Paper name not in database!")
+            else:
+                papers.pop(i)
+                print("Paper '%s' removed!" % paperName)
+
+        elif action == '4':
+
+            # Revert back to original paper list
+            print("Reverting back to original list!")
+            papers = list(originalPapers)
+
+        else:
+            print("Goodbye!\n")
+            break
+
     save_papers(papers)
     
 
